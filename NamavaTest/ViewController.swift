e9d257307db87446f499c157a9cb7994d1dbc93c
myrad.akbari <myrad.akbari@gmail.com>
2022-07-23 09:29:30 +0430

    Initial Commit

---
+++ NamavaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamavaTest
+//
+//  Created by reza akbari on 7/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
